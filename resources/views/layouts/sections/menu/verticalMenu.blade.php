5600ad0a149fb839f58642795aff7e560668bb88
Zain <masterzera111@gmail.com>
2025-07-19 22:38:17 +0700

    crud

--- resources/views/layouts/sections/menu/verticalMenu.blade.php
+++ resources/views/layouts/sections/menu/verticalMenu.blade.php
@@ -57,7 +57,7 @@
             <i class="{{ $menu->icon }}"></i>
           @endisset
           <div>{{ isset($menu->name) ? __($menu->name) : '' }}</div>
-          @if(isset($menu->badge) && $menu->name == 'Produk')
+          @if($menu->name == 'Produk' && $need_to_kulak_products > 0)
             <div class="badge rounded-pill bg-danger text-uppercase ms-auto">{{ $need_to_kulak_products }}</div>
           @endif
         </a>
