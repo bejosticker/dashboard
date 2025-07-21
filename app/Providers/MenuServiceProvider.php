3999e7729901a436ec7ed00e700eff5b831a67bd
Zain <masterzera111@gmail.com>
2025-07-21 14:27:00 +0700

    a

--- app/Providers/MenuServiceProvider.php
+++ app/Providers/MenuServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\View;
 use Illuminate\Routing\Route;
 use Illuminate\Support\ServiceProvider;
 use App\Models\Product;
+use Illuminate\Support\Facades\Schema;
 
 class MenuServiceProvider extends ServiceProvider
 {
@@ -24,10 +25,14 @@ class MenuServiceProvider extends ServiceProvider
   {
     $verticalMenuJson = file_get_contents(base_path('resources/menu/verticalMenu.json'));
     $verticalMenuData = json_decode($verticalMenuJson);
-    $count = Product::whereColumn('stock_cm', '<', 'minimum_stock_cm')->count();
+    if (Schema::hasTable('products')) {
+      $count = Product::whereColumn('stock_cm', '<', 'minimum_stock_cm')->count();
+    }
 
     // Share all menuData to all the views
     $this->app->make('view')->share('menuData', [$verticalMenuData]);
-    $this->app->make('view')->share('need_to_kulak_products', $count);
+    if (Schema::hasTable('products')) {
+      $this->app->make('view')->share('need_to_kulak_products', $count);
+    }
   }
 }
