49b259864838f198e1a815077dcc88fe3fe93f3b
Zain <masterzera111@gmail.com>
2025-07-24 14:43:49 +0700

    online

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -122,7 +122,7 @@
                         <div class="col-lg-6">
                             <label class="form-label">Stok Minimal</label>
                             <div class="input-group input-group-merge">
-                                <input type="number" value="{{ $product->minimum_stock_cm / $product->per_roll_cm / 100 }}" class="form-control" name="minimum_stock_cm"required placeholder="10">
+                                <input type="number" value="{{ $product->minimum_stock_cm / $product->per_roll_cm }}" class="form-control" name="minimum_stock_cm"required placeholder="10">
                                 <span class="input-group-text">Roll</span>
                             </div>
                         </div>
